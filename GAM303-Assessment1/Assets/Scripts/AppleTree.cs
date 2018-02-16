fa2450405bb0368a5f2407d830fb4319feb47af3
Zalron <33308793+Zalron@users.noreply.github.com>
2018-02-16 13:47:55 +1100

    Version 0.03
    Added moving tree

--- GAM303-Assessment1/Assets/Scripts/AppleTree.cs
+++ GAM303-Assessment1/Assets/Scripts/AppleTree.cs
@@ -2,15 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AppleTree : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
+public class AppleTree : MonoBehaviour
+{
+    public GameObject applePrefab; //Prefab for instantiating apples
+    public float speed = 10f; // Speed at which the AppleTree moves ion m/s
+    public float leftAndRightEdge = 10f; // Distance where AppleTree truns around
+    public float chanceToChangeDiections = 0.02f; // Chance that the AppleTree will change directions 
+    public float secondsBetweenAppleDrops = 1f; // Rate at whiich Apples will be instantiated
+    void Start () // Use this for initialization
+    {
+		//Dropping Apples every second
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	void Update () // Update is called once per frame
+    {
+        //Basic Movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+        //Changing Direction
+        if (pos.x < -leftAndRightEdge) // Move right
+        {
+            speed = Mathf.Abs(speed);
+        }
+        else if (pos.x > leftAndRightEdge) // Move left
+        {
+            speed = -Mathf.Abs(speed);
+        }
 	}
+    void FixedUpdate()
+    {
+        if (Random.value < chanceToChangeDiections) // Chage direction Randomly
+        {
+            speed *= -1;
+        }
+    }
 }
