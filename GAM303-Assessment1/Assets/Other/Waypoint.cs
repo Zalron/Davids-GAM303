275176c8580827a46ec0dd8754192de846686119
Zalron <33308793+Zalron@users.noreply.github.com>
2018-02-22 15:54:50 +1100

    Version 0.04.2

---
+++ GAM303-Assessment1/Assets/Other/Waypoint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour 
+{
+	Waypointholder waypointholder;
+	public void Start()
+	{
+		waypointholder = GameObject.Find ("WaypointHolder").GetComponent<Waypointholder> ();
+	}
+	public void OnTriggerEnter (Collider col)
+	{
+		if (col.name == "ColliderBottom") 
+		{
+			waypointholder.Updatescore ();
+		}
+	}
+}
---
+++ GAM303-Assessment1/Assets/Other/Waypointholder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Waypointholder : MonoBehaviour 
+{
+	public static float timer;
+	public static float score;
+	public Text scoreText;
+	public void Updatescore()
+	{
+		score += 100;
+		scoreText.text = score.ToString ();
+	}
+}
